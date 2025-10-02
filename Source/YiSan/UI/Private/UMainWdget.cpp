261798a193bdb9bd72ddcd3722b7618c8f46b4b2
dopple <kuberin_b@naver.com>
2025-10-02 16:01:49 +0900

    bot test class

---
+++ Source/YiSan/UI/Private/UMainWdget.cpp
@@ -0,0 +1,50 @@
+#include "UMainWdget.h"
+
+#include "YiSan.h"
+#include "GameLogging.h"
+#include "Components/EditableTextBox.h"
+#include "LatteLibrary/Character/Public/APlayerActor.h"
+#include "LatteLibrary/Character/Public/UVoiceCaptureComponent.h"
+
+void UMainWdget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (VoiceInputBox)
+	{
+		VoiceInputBox->OnTextCommitted.AddDynamic(this, &UMainWdget::HandleTextCommitted);
+	}
+
+	UE_LOG(LogYiSan, Verbose, TEXT("UMainWdget::NativeConstruct"));
+}
+
+void UMainWdget::HandleTextCommitted(const FText& Text, ETextCommit::Type CommitMethod)
+{
+	if (CommitMethod != ETextCommit::OnEnter)
+	{
+		return;
+	}
+
+	FString CleanText = Text.ToString();
+	CleanText.TrimStartAndEndInline();
+
+	if (CleanText.IsEmpty())
+	{
+		return;
+	}
+
+	PRINTLOG(TEXT("[UMainWdget] Enter committed text: %s"), *CleanText);
+
+	if (APlayerActor* PlayerActor = Cast<APlayerActor>(GetOwningPlayerPawn()))
+	{
+		if (UVoiceCaptureComponent* VoiceComponent = PlayerActor->FindComponentByClass<UVoiceCaptureComponent>())
+		{
+			VoiceComponent->RequestSynthesis(CleanText);
+		}
+	}
+
+	if (VoiceInputBox)
+	{
+		VoiceInputBox->SetText(FText::GetEmpty());
+	}
+}
