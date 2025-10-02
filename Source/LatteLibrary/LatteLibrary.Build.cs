e09705ce5ae0ae781965e15a8bfd81efed7fe498
dopple <kuberin_b@naver.com>
2025-10-02 16:27:44 +0900

    코드 수정

--- Source/LatteLibrary/LatteLibrary.Build.cs
+++ Source/LatteLibrary/LatteLibrary.Build.cs
@@ -22,6 +22,10 @@ public class LatteLibrary : ModuleRules
 			"HTTP",
 			"Json",
 			"JsonUtilities",
+			"Voice",
+			"AudioMixer",
+			"AudioCapture",
+			"AudioPlatformConfiguration",
 			"WebSockets",
 			"DeveloperSettings"
 		});
