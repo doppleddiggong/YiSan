009187dbdf870113f448646aa84ad333ab1898c5
dopple <kuberin_b@naver.com>
2025-10-14 21:17:17 +0900

    빌딩 데이터 갱신

--- Source/LatteLibrary/Data/Public/EBuildingType.h
+++ Source/LatteLibrary/Data/Public/EBuildingType.h
@@ -7,12 +7,12 @@ enum class EBuildingType : uint8
 	
 	Yeomingak      		UMETA(DisplayName = "여민각"),
 	Bongsudang     		UMETA(DisplayName = "봉수당"),
-	Naknamhyeon    		UMETA(DisplayName = "낙남현"),
+	Naknamhyeon    		UMETA(DisplayName = "낙남헌"),
 	Yuyotaek       		UMETA(DisplayName = "유여택"),
 	Jwaikmun      		UMETA(DisplayName = "좌익문"),
 	Uhwagwan      		UMETA(DisplayName = "우화관"),
 	Byeolju       		UMETA(DisplayName = "별주"),
-	Bijangcheol   		UMETA(DisplayName = "비장철"),
+	Bijangcheol   		UMETA(DisplayName = "비장청"),
 	Seoricheong   		UMETA(DisplayName = "서리청"),
 	Namgunyeong   		UMETA(DisplayName = "남군영"),
 	Sinpungnu     		UMETA(DisplayName = "신풍루"),
@@ -20,7 +20,7 @@ enum class EBuildingType : uint8
 	Bukgunyeong   		UMETA(DisplayName = "북군영"),
 	Eojeong       		UMETA(DisplayName = "어정"),
 	Iancheong     		UMETA(DisplayName = "이안청"),
-	Hyangdaecheong		UMETA(DisplayName = "향대청"),
+	Hyangdaecheong		UMETA(DisplayName = "행대청"),
 	Unhangak			UMETA(DisplayName = "운한각"),
 	Jaesil				UMETA(DisplayName = "재실"),
 
