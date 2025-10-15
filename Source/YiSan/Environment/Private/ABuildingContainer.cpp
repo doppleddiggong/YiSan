801682813fa792ba8c73fcda9a1482a4771dcd4d
dopple <kuberin_b@naver.com>
2025-10-15 17:57:57 +0900

    건물 컨테이너 세팅

---
+++ Source/YiSan/Environment/Private/ABuildingContainer.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2025 Doppleddiggong. All rights reserved. Unauthorized copying, modification, or distribution of this file, via any medium is strictly prohibited. Proprietary and confidential.
+
+
+#include "ABuildingContainer.h"
+
+ABuildingContainer::ABuildingContainer()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ABuildingContainer::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/YiSan/Environment/Public/ABuildingContainer.h
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2025 Doppleddiggong. All rights reserved. Unauthorized copying, modification, or distribution of this file, via any medium is strictly prohibited. Proprietary and confidential.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ABuildingContainer.generated.h"
+
+UCLASS()
+class YISAN_API ABuildingContainer : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABuildingContainer();
+
+protected:
+	virtual void BeginPlay() override;
+};
