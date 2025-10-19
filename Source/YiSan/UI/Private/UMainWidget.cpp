7022e89836e62b1812d54695511f8ced81f209fa
jjammim0209 <woals1375@naver.com>
2025-10-19 18:10:47 +0900

    [map] mega popup 수정

--- Source/YiSan/UI/Private/UMainWidget.cpp
+++ Source/YiSan/UI/Private/UMainWidget.cpp
@@ -180,24 +180,7 @@ void UMainWidget::ToggleMegaPopup()
 		{
 			Pawn->DisableInput(PC);
 		}
-
-		if (CurNearBuildingType != EBuildingType::None)
-		{
-			// enum 의 건물 가져오기
-			FString BuildingName = StaticEnum<EBuildingType>()->GetDisplayNameTextByValue((int32)CurNearBuildingType).ToString();
-			// enum 값 확인용
-			FString Prompt = FString::Printf(TEXT("%s에 대해 자세히 설명해줘."), *BuildingName);
-			PRINTLOG(TEXT("현재 건물은:%s"), *BuildingName);
-			
-			FGPTContext SpatialContext = BuildSpatialContext();
-			// 생성해서 함수에 response한다
-			if (auto ReqNetwork = UHttpNetworkSystem::Get(GetWorld()))
-			{
-				ReqNetwork->RequestGPT(Prompt, SpatialContext, 
-					FResponseAskDelegate::CreateUObject(this, &UMainWidget::OnMegaPopupResponse));
-			}
-		}
-
+		
 		// 시네마틱 뷰 활성화 할떄 사용할꺼
 		// PC->SetCinematicMode(true, false, false, true, true);
 
@@ -256,27 +239,6 @@ void UMainWidget::OnNearBuildingBroadcast(EBuildingType BuildingType)
     {
         SmallPopupCtn->SetVisibility(ESlateVisibility::Collapsed);
     }
-}
-
-// gpt 의 대답을 처리할 함수를 생성하자
-void UMainWidget::OnMegaPopupResponse(FResponseAsk& Response, bool bSuccess)
-{
-	//megapopup 이 아니라면 리턴하자
-	if (!MegaPopupCtn)
-		return;
-	// 값이 들어왔다면
-	if (bSuccess)
-	{
-		PRINTLOG(TEXT("수신된 대답은: %s"),*Response.gpt_response_text);
-        
-		
-	}
-	// 만약 실패했다면
-	else
-	{
-		PRINTLOG(TEXT("GPT 에게 응답이 됮 않았습니다"));
-		
-	}
 }
 
 // 팝업 눌렀을때 닫히게 하고싶다
