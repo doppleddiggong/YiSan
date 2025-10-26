98a3ede6c5b9ef68b44bfacaf11a4e404ed4ddbc
dopple <kuberin_b@naver.com>
2025-10-26 10:04:33 +0900

    feat : main UI 위치 조정 및 불필요 주석 제거

--- Source/YiSan/UI/Private/UMainWidget.cpp
+++ Source/YiSan/UI/Private/UMainWidget.cpp
@@ -1,31 +1,18 @@
 // Copyright (c) 2025 Doppleddiggong. All rights reserved. Unauthorized copying, modification, or distribution of this file, via any medium is strictly prohibited. Proprietary and confidential.
 
 #include "UMainWidget.h"
-#include "GameLogging.h"
 #include "UBroadcastManager.h"
-#include "UHttpNetworkSystem.h"
-#include "APlayerActor.h"
-#include "FGPTContext.h"
-#include "UVoiceFunctionLibrary.h"
 
 #include "USmallPopup.h"
 #include "UMegaPopup.h"
 
-#include "Components/CanvasPanel.h"
 #include "Components/EditableTextBox.h"
-#include "Kismet/GameplayStatics.h"
-#include "Sound/SoundWaveProcedural.h"
 #include "Input/Reply.h"
 
 void UMainWidget::NativeConstruct()
 {
     Super::NativeConstruct();
 
-    // if (InputText)
-    //     InputText->OnTextCommitted.AddDynamic(this, &UMainWidget::OnMessageComitted);
-    //
-    // ChatBox->SetVisibility(ESlateVisibility::Hidden);
-    
     if ( APlayerController* PC = GetWorld()->GetFirstPlayerController() )
     {
         FInputModeGameOnly InputMode;
@@ -50,42 +37,6 @@ void UMainWidget::NativeConstruct()
         MegaPopupCtn->SetVisibility(ESlateVisibility::Collapsed);
 }
 
-//
-// void UMainWidget::ToggleChatBox()
-// {
-//     // if (ChatBox->GetVisibility() == ESlateVisibility::Hidden)
-//     // {
-//     //     InputText->SetText(FText::GetEmpty());
-//     //     ChatBox->SetVisibility(ESlateVisibility::Visible);
-//     //
-//     //     if (InputText)
-//     //         InputText->SetKeyboardFocus();
-//     //
-//     //     if (APlayerController* PC = GetOwningPlayer())
-//     //     {
-//     //         FInputModeGameAndUI InputMode;
-//     //         InputMode.SetWidgetToFocus(InputText->TakeWidget());
-//     //         InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
-//     //
-//     //         PC->SetInputMode(InputMode);
-//     //         PC->SetShowMouseCursor(true);
-//     //     }
-//     // }
-//     // else
-//     // {
-//     //     SendChatMessage( InputText->GetText().ToString());
-//     //     ChatBox->SetVisibility(ESlateVisibility::Hidden);
-//     //
-//     //     if (APlayerController* PC = GetOwningPlayer())
-//     //     {
-//     //         FInputModeGameOnly InputMode;
-//     //         InputMode.SetConsumeCaptureMouseDown(false);
-//     //     
-//     //         PC->SetInputMode(InputMode);
-//     //         PC->SetShowMouseCursor(false);
-//     //     }
-//     // }
-// }
 
 bool UMainWidget::IsMegaPopupVisible() const
 {
@@ -100,15 +51,6 @@ bool UMainWidget::IsSmallPopupVisible() const
 		return false;
     return SmallPopupCtn->GetVisibility() == ESlateVisibility::Visible;
 }
-
-// void UMainWidget::OnMessageComitted(const FText& Text, ETextCommit::Type CommitMethod)
-// {
-//     if (CommitMethod == ETextCommit::OnEnter)
-//     {
-//         ToggleChatBox();
-//     }
-// }
-
 
 void UMainWidget::OnMegaPopupClosed()
 {
