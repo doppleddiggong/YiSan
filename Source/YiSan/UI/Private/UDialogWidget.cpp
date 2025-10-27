2cb4a04443dcec5f6b19725b5190df01db5c506d
dopple <kuberin_b@naver.com>
2025-10-27 11:48:16 +0900

    위젯 타이머 타이밍 조정

--- Source/YiSan/UI/Private/UDialogWidget.cpp
+++ Source/YiSan/UI/Private/UDialogWidget.cpp
@@ -20,16 +20,29 @@ void UDialogWidget::ShowDialog(FString InString)
 	if (!DialogBorder || !DialogText)
 		return;
 
+	// 이전 타이머가 있으면 취소
+	if (UWorld* World = GetWorld())
+	{
+		if (HideTimerHandle.IsValid())
+			World->GetTimerManager().ClearTimer(HideTimerHandle);
+	}
+
 	DialogBorder->SetVisibility(ESlateVisibility::Visible);
 	DialogBorder->SetBrushColor(ActivateColor);
 	DialogText->SetText(FText::FromString(InString));
 
-	if (auto DelayManager = UDelayTaskManager::Get(GetWorld()))
+	// 5초 후 숨기는 타이머 설정
+	if (UWorld* World = GetWorld())
 	{
-		DelayManager->Delay(this, 5, [&]()
-		{
-			DialogText->SetText(FText::GetEmpty());
-			DialogBorder->SetBrushColor(DeactivateColor);
-		});
+		World->GetTimerManager().SetTimer(
+			HideTimerHandle,
+			[this]()
+			{
+				DialogText->SetText(FText::GetEmpty());
+				DialogBorder->SetBrushColor(DeactivateColor);
+			},
+			5.0f,
+			false
+		);
 	}
 }
