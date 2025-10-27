0133bc63358cd28eca58e72587bcf0475a13f396
dopple <kuberin_b@naver.com>
2025-10-27 14:16:08 +0900

    워닝 수정

--- Source/YiSan/Loading/Private/YiSanPlayerListManager.cpp
+++ Source/YiSan/Loading/Private/YiSanPlayerListManager.cpp
@@ -10,6 +10,7 @@
 AYiSanPlayerListManager::AYiSanPlayerListManager()
 {
     bReplicates = true;
+    bAlwaysRelevant = true; // 이 줄을 추가합니다.          
     SetReplicatingMovement(false);
     PrimaryActorTick.bCanEverTick = false;
 }
