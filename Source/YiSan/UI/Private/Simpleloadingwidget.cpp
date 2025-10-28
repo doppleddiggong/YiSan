5f18f84e5977b8c9a691d6875520f3ffea3bedf7
jjammim0209 <woals1375@naver.com>
2025-10-28 17:52:48 +0900

    [UI] loading 관련 UI 만들어뒀던거 다시 만들었습니다 (깃 하면서 날라갔던 부분)

---
+++ Source/YiSan/UI/Private/Simpleloadingwidget.cpp
@@ -0,0 +1,63 @@
+﻿// Copyright (c) 2025 Doppleddiggong. All rights reserved.
+
+#include "SimpleLoadingWidget.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+
+void USimpleLoadingWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (LoadingProgressBar)
+	{
+		LoadingProgressBar->SetPercent(0.0f);
+	}
+
+	if (StatusText)
+	{
+		StatusText->SetText(FText::FromString(TEXT("로딩 중...")));
+	}
+
+	if (ProgressText)
+	{
+		ProgressText->SetText(GetPercentageText(0.0f));
+	}
+}
+
+void USimpleLoadingWidget::UpdateProgress(float Progress, const FText& StatusTextValue)
+{
+	Progress = FMath::Clamp(Progress, 0.0f, 1.0f);
+
+	if (LoadingProgressBar)
+	{
+		LoadingProgressBar->SetPercent(Progress);
+		
+		// 진행률에 따라 색상 변경
+		FLinearColor Color;
+		if (Progress < 0.5f)
+		{
+			Color = FLinearColor::LerpUsingHSV(FLinearColor::Red, FLinearColor::Yellow, Progress * 2.0f);
+		}
+		else
+		{
+			Color = FLinearColor::LerpUsingHSV(FLinearColor::Yellow, FLinearColor::Green, (Progress - 0.5f) * 2.0f);
+		}
+		LoadingProgressBar->SetFillColorAndOpacity(Color);
+	}
+
+	if (ProgressText)
+	{
+		ProgressText->SetText(GetPercentageText(Progress));
+	}
+
+	if (StatusText)
+	{
+		StatusText->SetText(StatusTextValue);
+	}
+}
+
+FText USimpleLoadingWidget::GetPercentageText(float Progress) const
+{
+	int32 Percentage = FMath::RoundToInt(Progress * 100.0f);
+	return FText::FromString(FString::Printf(TEXT("%d%%"), Percentage));
+}
