92a6f0e0ebbacfb24287655605dca6409e6b78a3
jjammim0209 <woals1375@naver.com>
2025-11-02 13:02:46 +0900

    [main] 메인머지

---
+++ Source/YiSan/Loading/Public/UYisanOnlineSystem.h
@@ -0,0 +1,81 @@
+﻿// Copyright (c) 2025 Doppleddiggong. All rights reserved. Unauthorized copying, modification, or distribution of this file, via any medium is strictly prohibited. Proprietary and confidential.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Macro.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "UYisanOnlineSystem.generated.h"
+
+DECLARE_DELEGATE_TwoParams(FFindComplete, int32, FString);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerListUpdated, const TArray<FString>& /* PlayerNames */);
+
+UCLASS()
+class YISAN_API UYisanOnlineSystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	DEFINE_SUBSYSTEM_GETTER_INLINE(UYisanOnlineSystem);
+
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	
+public:
+	FFindComplete OnFindComplete;
+
+	//세션의 모든 처리 진행 객체
+	IOnlineSessionPtr sessionInterface;
+
+	//세션 생성 
+	//세션 생성 함수
+	UFUNCTION(BlueprintCallable)
+	void CreateMySession(FString displayName, int32 playerCount);
+	//세션 생성 완료 함수
+	void OnCreateSessionComplete(FName sessionName, bool success);
+
+	//세션 조회 
+	//세션 조회할 때 사용하는 객체
+	TSharedPtr<FOnlineSessionSearch> sessionSearch;
+	//세션 조회 함수
+	UFUNCTION(BlueprintCallable)
+	void FindOtherSession();
+	//세션 조회 완료 함수
+	void OnFindSessionComplete(bool success);
+
+	//세션 참여 
+	//세션 참여 함수
+	UFUNCTION(BlueprintCallable)
+	void JoinOtherSession(int32 sessionIndex);
+	//세션 참여 완료 함수
+	void OnJoinSessionComplete(FName sessionName, EOnJoinSessionCompleteResult::Type result);
+
+	// IP로 세션 참여 함수
+	UFUNCTION(BlueprintCallable)
+	void JoinSessionByIp(const FString& IpAddress);
+
+	//=============이름 저장 부분================
+public:
+	void SetPlayerNickname(const FString& InName);
+	FString GetPlayerNickname() { return PlayerNickname; }
+
+private:
+	FString PlayerNickname;
+
+
+	//=============플레이어 목록 관리 부분================
+public:
+	FOnPlayerListUpdated OnPlayerListUpdated;
+
+	UFUNCTION(BlueprintCallable)
+	void RequestRefreshPlayerList();
+
+	void HandlePlayerListUpdated(const TArray<FString>& PlayerNames);
+	
+	void SetGameState(class AYisanGameState* InGameState);
+	class AYisanGameState* GetGameState() const { return CachedGameState; }
+
+private:
+	UPROPERTY()
+	TObjectPtr<class AYisanGameState> CachedGameState;
+};
